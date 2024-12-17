bf5a1976bcfd862354a74f918d6b6a7705d83d04
Luca Belardinelli <belardinelliluca@gmail.com>
2024-12-17 16:23:11 +0100

    script biglietto

---
+++ script.js
@@ -0,0 +1,13 @@
+// js-biglietto-treno
+
+// INPUT
+// DOMANDA N-1 DA FARE ALL'UTENTE
+let km = prompt("indicare i km da percorrere");
+
+// DOMANDA N-1 DA FARE ALL'UTENTE
+let eta = prompt("indicare età");
+
+console.log(km, eta);
+
+
+// ELABORAZIONE DELLO SCONTO IN BASE ALL'ETà
